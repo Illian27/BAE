72054d2601cf9e66ab610a6caba51f1a9872506b
Ildegar <illianortegap@gmail.com>
2024-04-19 17:44:15 +0100

    asdhfsfdhaiadfsda

---
+++ nombre.js
@@ -0,0 +1,92 @@
+use media;
+
+// Ejercicio 1
+db.media.insertMany([{
+        tipo: "libro",
+        titulo: "Java para todos",
+        ISBN: "987-1-2344-5334-8",
+        editorial: "Anaya",
+        Autor: ["Peper Caballero", "Isabel Sanz", "Timoteo Marino"],
+        capitulos: 10
+    },
+
+    {
+        capitulo: 1,
+        titulo: "Primeros pasos en JAVA",
+        longitud: 20
+    },
+
+    {
+        capitulo: 2,
+        titulo: "Primeros pasos en JAVA",
+        longitud: 25
+    },
+
+    {
+        tipo: "CD",
+        Artista: "Los piratas",
+        titulo: "Recuerdos",
+        canciones: [{
+                cancion: 1,
+                titulo: "Adios mi barco",
+                longitud: "3:20"
+            },
+            {
+                cancion: 2,
+                titulo: "Pajaritos",
+                longitud: "4:15"
+            }
+        ]
+    },
+
+    {
+        tipo: "DVD",
+        titulo: "Matrix",
+        estreno: 1999,
+        actores: ["keanu Reeves", "carry-Anne Moss", "Laurence Fishburne", "Hugo Veaving", "Gloria Foster", "Joe Pantoliano"]
+    }
+]);
+
+// Ejercicio 2
+db.media.updateOne(
+    {titulo:"Matrix"},
+    {$set:{tipo: "DVD", titulo: "Matrix", estreno: 1999, genero: "accion"}}
+);
+
+// Ejercicio 3
+db.media.insertOne({
+tipo:"Libro",
+titulo:"Constantinopla",
+capitulos:12,
+leidos:3
+});
+
+db.media.updateOne(
+    {titulo:"Constantinopla"},
+    {$inc:{leidos:5}}
+);
+
+// Ejercicio 4
+db.media.updateOne(
+    {titulo:"Matrix"},
+    {$set:{genero:"ciencia ficción"}}
+);
+
+// Ejercicio 5
+db.media.updateOne(
+    {titulo:"Java para todos"},
+    {$unset:{editorial:"Anaya"}}
+);
+
+// Ejercicio 6
+db.media.updateOne(
+    {titulo:"Java para todos"},
+    {$push:{Autor:"María Sancho"}}
+);
+
+// 7
+db.media.updateOne(
+    {titulo:"Matrix"},
+    {$push:{actores:{$each:["Antonio Banderas", "Antonio Banderas"]}}}
+);
+db.media.find();
