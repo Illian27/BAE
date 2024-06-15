ac69c05e976330af9c3c4082b0fa6d7332079c09
Santi <98534635+Illian27@users.noreply.github.com>
2024-06-15 17:10:58 +0100

    Actualizar Inserciones-Consultas.sql

--- Vitaloptics/Inserciones-Consultas.sql
+++ Vitaloptics/Inserciones-Consultas.sql
@@ -26,150 +26,66 @@ INSERT INTO interno(trabajador_id, DNI, telefono, correo, num_cuenta) VALUES
 (2, '23456789B', '555-5678', 'maria@example.com', 'ES7620770024003102575767'),
 (3, '34567890C', '555-8765', 'carlos@example.com', 'ES7620770024003102575768');
 
-
 INSERT INTO externo(trabajador_id, profesion, empresa) VALUES
 (1, 'Consultor', 'ABC Consulting'),
 (2, 'Ingeniero', 'XYZ Engineering'),
 (3, 'Analista', 'Data Solutions');
-
 
 INSERT INTO pago(num_tarjeta, fecha, total) VALUES
 ('1234-5678-9012-3456', '2024-06-01', 100.50),
 ('2345-6789-0123-4567', '2024-06-02', 200.75),
 ('3456-7890-1234-5678', '2024-06-03', 150.00);
 
-CREATE TABLE factura (
-  id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  nombre_trabajador VARCHAR(15) NOT NULL,
-  fecha DATETIME NOT NULL,
-  direccion VARCHAR(50) NOT NULL,
-  idcliente INT UNSIGNED NOT NULL,
-  nombre_cliente VARCHAR(15),
-  total INT UNSIGNED NOT NULL,
-  pago_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_factura_pago FOREIGN KEY(pago_id) REFERENCES pago(id) ON DELETE  RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (id, pago_id));
 INSERT INTO factura(nombre_trabajador, fecha, direccion, idcliente, nombre_cliente, total, pago_id) VALUES
 ('Juan Perez', '2024-06-01', 'Calle Mayor 1', 1, 'Carlos Martinez', 100.50, 1),
 ('Maria Rodriguez', '2024-06-02', 'Calle Menor 2', 2, 'Ana Lopez', 200.75, 2),
 ('Carlos Sanchez', '2024-06-03', 'Calle Central 3', 3, 'Luis Gomez', 150.00, 3);
 
-CREATE TABLE producto (
-  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  nombre VARCHAR(15) NOT NULL,
-  precio FLOAT4 UNSIGNED NOT NULL,
-  color VARCHAR(12) NOT NULL,
-  dimenciones VARCHAR(50) NOT NULL,
-  stock INT NOT NULL
-);
 INSERT INTO producto(nombre, precio, color, dimenciones, stock) VALUES
 ('Silla', 49.99, 'Rojo', '40x40x90 cm', 10),
 ('Mesa', 89.99, 'Blanco', '120x60x75 cm', 5),
 ('Lámpara', 29.99, 'Azul', '30x30x50 cm', 20);
 
-CREATE TABLE servicio (
-  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  nombre VARCHAR(15) NOT NULL,
-  precio FLOAT4 NOT NULL,
-  detalles TEXT NOT NULL,
-  tipo ENUM('Óptica', 'Audiología') NOT NULL
-);
 INSERT INTO servicio(nombre, precio, detalles, tipo) VALUES
 ('Limpieza', 25.00, 'Limpieza general', 'Óptica'),
 ('Mantenimiento', 50.00, 'Revisión y reparación de audifonos', 'Audiología'),
 ('Consultoría', 100.00, 'Asesoría en audioción', 'Audiología');
 
--- Tablas de normalizaciones
-CREATE TABLE interno_habilidades (
-  interno_trabajador_id INT UNSIGNED PRIMARY KEY NOT NULL,
-  habilidad VARCHAR(20) NOT NULL,
-  CONSTRAINT fk_interno_habilidades_interno1 FOREIGN KEY(interno_trabajador_id) REFERENCES interno(trabajador_id) ON DELETE  CASCADE ON UPDATE CASCADE
-);
 INSERT INTO interno_habilidades(interno_trabajador_id, habilidad) VALUES
 (1, 'Comprensión'),
 (2, 'Paciencia'),
 (3, 'Tacto');
 
--- Tablas de relaciones
-CREATE TABLE es_jefe_de (
-  trabajador_id INT UNSIGNED NOT NULL,
-  jefe_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_jefe FOREIGN KEY(jefe_id) REFERENCES trabajador(id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_es_jefe_de_trabajador1 FOREIGN KEY(trabajador_id) REFERENCES trabajador(id) ON DELETE  CASCADE ON UPDATE RESTRICT,
-PRIMARY KEY(trabajador_id, jefe_id));
 INSERT INTO es_jefe_de(trabajador_id, jefe_id) VALUES
 (1, 2),
 (2, 3),
 (3, 1);
 
-CREATE TABLE promociona (
-  registrado_cliente_id INT UNSIGNED NOT NULL,
-  cliente_id INT UNSIGNED NOT NULL,
-  codigo CHAR(5) NOT NULL,
-  CONSTRAINT fk_registrado_has_cliente_registrado1 FOREIGN KEY(registrado_cliente_id) REFERENCES registrado(cliente_id) ON DELETE  RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT fk_registrado_has_cliente_cliente1 FOREIGN KEY(cliente_id) REFERENCES cliente(id) ON DELETE  CASCADE ON UPDATE CASCADE,
-PRIMARY KEY (registrado_cliente_id, cliente_id));
 INSERT INTO promociona(registrado_cliente_id, cliente_id, codigo) VALUES
 (1, 2, 'AB123'),
 (2, 3, 'DE456'),
 (3, 1, 'GH789');
 
-CREATE TABLE realiza (
-  registrado_cliente_id INT UNSIGNED NOT NULL,
-  pago_id INT UNSIGNED NOT NULL,
-  oferta INT NULL,
-  CONSTRAINT fk_realiza_registrado1 FOREIGN KEY(registrado_cliente_id) REFERENCES registrado(cliente_id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_realiza_pago1 FOREIGN KEY(pago_id) REFERENCES pago(id) ON DELETE  RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (registrado_cliente_id, pago_id));
 INSERT INTO realiza(registrado_cliente_id, pago_id, oferta) VALUES
 (1, 1, '10'),
 (2, 2, '50'),
 (3, 3, '100');
 
-CREATE TABLE hace (
-  no_registrado_cliente_id INT UNSIGNED NOT NULL,
-  pago_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_hace_no_registrado1 FOREIGN KEY(no_registrado_cliente_id) REFERENCES no_registrado(cliente_id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_hace_pago1 FOREIGN KEY(pago_id) REFERENCES pago(id) ON DELETE  CASCADE ON UPDATE CASCADE,
-PRIMARY KEY (no_registrado_cliente_id, pago_id));
 INSERT INTO hace(no_registrado_cliente_id, pago_id) VALUES
 (1, 1),
 (2, 2),
 (3, 3);
 
-CREATE TABLE tramita (
-  trabajador_id INT UNSIGNED NOT NULL,
-  cliente_id INT UNSIGNED NOT NULL,
-  factura_id INT UNSIGNED NOT NULL,
-  factura_pago_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_tramita_trabajador1 FOREIGN KEY(trabajador_id) REFERENCES trabajador(id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_tramita_cliente1 FOREIGN KEY(cliente_id) REFERENCES cliente(id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_tramita_factura1 FOREIGN KEY(factura_id , factura_pago_id) REFERENCES factura(id , pago_id) ON DELETE  RESTRICT ON UPDATE RESTRICT,
-PRIMARY KEY (trabajador_id, cliente_id));
 INSERT INTO tramita(trabajador_id, cliente_id, factura_id, factura_pago_id) VALUES
 (1, 1, 1, 1),
 (2, 2, 2, 2),
 (3, 3, 3, 3);
 
-CREATE TABLE tiene (
-  factura_id INT UNSIGNED NOT NULL,
-  factura_pago_id INT UNSIGNED NOT NULL,
-  producto_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_factura_has_producto_factura1 FOREIGN KEY(factura_id , factura_pago_id) REFERENCES factura(id , pago_id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_factura_has_producto_producto1 FOREIGN KEY(producto_id) REFERENCES producto(id) ON DELETE  RESTRICT ON UPDATE RESTRICT,
-PRIMARY KEY (factura_id, factura_pago_id, producto_id));
 INSERT INTO tiene(factura_id, factura_pago_id, producto_id) VALUES
 (1, 1, 1),
 (2, 2, 2),
 (3, 3, 3);
 
-CREATE TABLE contiene (
-  factura_id INT UNSIGNED NOT NULL,
-  factura_pago_id INT UNSIGNED NOT NULL,
-  servicio_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_factura_has_servicio_factura1 FOREIGN KEY(factura_id , factura_pago_id) REFERENCES factura(id , pago_id) ON DELETE  CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_factura_has_servicio_servicio1 FOREIGN KEY(servicio_id) REFERENCES servicio(id) ON DELETE  RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (factura_id, factura_pago_id, servicio_id));
 INSERT INTO contiene(factura_id, factura_pago_id, servicio_id) VALUES
 (1, 1, 1),
 (2, 2, 2),
